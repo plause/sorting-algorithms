f7652183984f60e0fc85257154ac69f2861e5dc9
Huazhou Wang <plause@gmail.com>
2014-12-01 22:22:10 +0800

    Add bubble sort

---
+++ src/bubble.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+void sort(int size, int *array)
+{
+  int i, j;
+
+  for (i = 0; i < size - 1; i++)
+    for (j = i + 1; j < size; j++)
+      if (array[i] > array[j])
+        swap(array + i, array + j);
+}
